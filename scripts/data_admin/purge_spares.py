ec31468aa17f1306c778756d9967b18f07751e4f
aengusl <aenguslynch@gmail.com>
2022-12-23 20:38:47 +0000

    created purge script, WIP

---
+++ scripts/data_admin/purge_spares.py
@@ -0,0 +1,37 @@
+import os
+from tqdm import tqdm
+from PIL import Image
+from datetime import datetime
+
+"""
+There are spare files in desert folder, so any images with count > 900 
+will be removed
+"""
+
+root = '/home/aengusl/Desktop/Projects/OOD_workshop/Stable_Diffusion_Generation/gen_images/all-iters_100_images/desert'
+for folder in os.listdir(root):
+    class_folder = os.path.join(root, folder)
+    for file in os.listdir()
+        filename = os.fsdecode(file)
+        if filename.endswith(".png"):
+
+
+# to be completed
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
