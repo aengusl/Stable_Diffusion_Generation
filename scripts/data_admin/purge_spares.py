12053133da81ef7797987f513a2d092012ccd7e1
aengusl <aenguslynch@gmail.com>
2022-12-24 09:54:47 +0000

    completed purge spares file and evened out all_iters_100 dataset to 900 images per comb

--- scripts/data_admin/purge_spares.py
+++ scripts/data_admin/purge_spares.py
@@ -11,12 +11,16 @@ will be removed
 root = '/home/aengusl/Desktop/Projects/OOD_workshop/Stable_Diffusion_Generation/gen_images/all-iters_100_images/desert'
 for folder in os.listdir(root):
     class_folder = os.path.join(root, folder)
-    for file in os.listdir()
+    for file in os.listdir(class_folder):
         filename = os.fsdecode(file)
         if filename.endswith(".png"):
+            count_png = filename.split('_')[-1]
+            count = int(count_png.split('.')[0])
+            if count > 900:
+                file_path = os.path.join(class_folder, file)
+                os.remove(file_path)
 
-
-# to be completed
+            
 
 
 
